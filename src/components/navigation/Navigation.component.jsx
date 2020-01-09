f57ed754555d03d4d4033d318cfeae3043b46e4a
Dariusz Okonski <darek200180@gmail.com>
2020-01-09 21:08:13 +0100

    wrap inner code into .container

--- src/components/navigation/Navigation.component.jsx
+++ src/components/navigation/Navigation.component.jsx
@@ -3,24 +3,27 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
-      <NavLink className="navbar-brand" exact to="/">DazzDev</NavLink>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
 
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <NavLink className="nav-link" exact to="/">Home</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" exact to="/game">Game</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" exact to="/about">About</NavLink>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
+      <div className="container">
+        <NavLink className="navbar-brand" exact to="/">DazzDev</NavLink>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/">Home</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/game">Game</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" exact to="/about">About</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
