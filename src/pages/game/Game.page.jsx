08a833222cb412423f2bd748a3246a6fd638c442
Dariusz Okonski <darek200180@gmail.com>
2020-01-10 00:06:26 +0100

    display NumberWrong component

--- src/pages/game/Game.page.jsx
+++ src/pages/game/Game.page.jsx
@@ -5,25 +5,26 @@ import imagesArr from '../../utillities/images';
 import LeftSection from '../../components/left-section/LeftSection.component';
 import AlphabetButtons from '../../components/alphabet-buttons/AlphabetButtons.component';
 import RestartButton from '../../components/restart-button/RestartButton.component';
+import NumberWrong from '../../components/number-wrong/NumberWrong.component';
 
 class Game extends Component {
   state = {
     numberWrong: 0,
     currentWord: 'plently',
-    guessedWord: '-l---ly',
+    guesseWord: '-l---ly',
   }
 
   render() {
-    const { numberWrong, currentWord, guessedWord } = this.state;
+    const { numberWrong, guesseWord } = this.state;
 
     return (
       <div className="game">
         <LeftSection img={imagesArr[numberWrong]} />
 
         <section className="right">
-          <h6>Number wrong: {numberWrong}</h6>
+          <NumberWrong numberWrong={numberWrong} />
 
-          <h4>{guessedWord}</h4>
+          <h4>{guesseWord}</h4>
 
           <AlphabetButtons />
 
