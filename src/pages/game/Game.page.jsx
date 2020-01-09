2dff35ef32aeb1643b74def86fd45e39e4b60e30
Dariusz Okonski <darek200180@gmail.com>
2020-01-09 22:35:54 +0100

    comment backgroud-color

--- src/pages/game/Game.page.jsx
+++ src/pages/game/Game.page.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import './Game.styles.scss';
+import img0 from '../../utillities/img/0.jpg';
+import LeftSection from '../../components/left-section/LeftSection.component';
 
 class Game extends Component {
-  state = {}
+  state = {
+    currentImg: img0
+  }
 
   render() {
+    const { currentImg } = this.state;
+
     return (
       <div className="game">
-        Game
+        <LeftSection img={currentImg} />
+
+        <section className="right">
+          right
+        </section>
       </div>
     );
   }
