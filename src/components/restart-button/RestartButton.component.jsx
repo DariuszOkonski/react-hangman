76936b7fc35c3143800a242ff836cd653882979f
Dariusz Okonski <darek200180@gmail.com>
2020-01-09 23:53:28 +0100

    add RestartButton component

---
+++ src/components/restart-button/RestartButton.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './RestartButton.styles.scss';
+
+const RestartButton = () => {
+  return (
+    <button className="restart-button">Restart?</button>
+  );
+}
+
+export default RestartButton;
