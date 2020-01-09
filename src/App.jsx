8da8aef53519534309cd1d183f6b3606fa615534
Dariusz Okonski <darek200180@gmail.com>
2020-01-09 21:03:33 +0100

    display Footer component at App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navigation from './components/navigation/Navigation.component';
+import Footer from './components/footer/Footer.component';
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <div className="App">
         <Navigation />
 
+        <h1>Hello world</h1>
+
+        <Footer />
       </div>
     </BrowserRouter>
   );
