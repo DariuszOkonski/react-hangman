82849b2dfbece506047c9d3ff7221c54cf600367
Dariusz Okonski <darek200180@gmail.com>
2020-01-09 21:04:51 +0100

    change Navbar header into DazzDev

--- src/components/navigation/Navigation.component.jsx
+++ src/components/navigation/Navigation.component.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-primary fixed-top">
-      <NavLink className="navbar-brand" exact to="/">Navbar</NavLink>
+      <NavLink className="navbar-brand" exact to="/">DazzDev</NavLink>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
