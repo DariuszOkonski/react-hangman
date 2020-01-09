d444d512b512a15240bef36b4147ca240e56dde4
Dariusz Okonski <darek200180@gmail.com>
2020-01-09 23:35:01 +0100

    add components AlphabetButtons and SingleButton

---
+++ src/components/alphabet-buttons/AlphabetButtons.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './AlphabetButtons.styles.scss';
+import SingleButton from './single-button/SingleButton.component';
+
+const AlphabetButtons = () => {
+  return (
+    <div className="alphabet-buttons">
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+      <SingleButton />
+    </div>
+  );
+}
+
+export default AlphabetButtons;
---
+++ src/components/alphabet-buttons/single-button/SingleButton.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SingleButton.styles.scss';
+
+const SingleButton = (props) => {
+  return (
+    <div className="single-button">
+      S
+    </div>
+  );
+}
+
+export default SingleButton;
