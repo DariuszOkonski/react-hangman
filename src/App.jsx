3c0c9ce6758db45db8fd3e1ec31a99f268afbb2e
Dariusz Okonski <darek200180@gmail.com>
2020-01-09 22:10:36 +0100

    display Game component and add container to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navigation from './components/navigation/Navigation.component';
 import Footer from './components/footer/Footer.component';
+import Game from './pages/game/Game.page';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="App container">
         <Navigation />
 
-        <h1>Hello world</h1>
+        <Game />
 
         <Footer />
       </div>
