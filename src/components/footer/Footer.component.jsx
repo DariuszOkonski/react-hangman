5d4ce0e0f7d9e80af8c54839d383e785c028f7b2
Dariusz Okonski <darek200180@gmail.com>
2020-01-09 21:01:17 +0100

    add Footer component

---
+++ src/components/footer/Footer.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="navbar navbar-dark justify-content-center bg-primary fixed-bottom">
+      <p style={{ margin: 0 }} className="text-white">2020 &copy; Dariusz Okonski</p>
+    </footer>
+  );
+}
+
+export default Footer;
