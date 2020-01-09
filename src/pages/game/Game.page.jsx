6cef87d4cfb7e2ef9c5f26b53b8a99029aaf35a1
Dariusz Okonski <darek200180@gmail.com>
2020-01-09 23:12:18 +0100

    add states currentWord and guessedWord and some html to section right

--- src/pages/game/Game.page.jsx
+++ src/pages/game/Game.page.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import './Game.styles.scss';
 import imagesArr from '../../utillities/images';
+// import { randomWord } from '../../utillities/words';
 import LeftSection from '../../components/left-section/LeftSection.component';
 
 class Game extends Component {
   state = {
-    numberWrong: 0
+    numberWrong: 0,
+    currentWord: 'plently',
+    guessedWord: '-l---ly',
   }
 
   render() {
-    const { numberWrong } = this.state;
+    const { numberWrong, currentWord, guessedWord } = this.state;
 
     return (
       <div className="game">
         <LeftSection img={imagesArr[numberWrong]} />
 
         <section className="right">
-          right
+          <h6>Number wrong: {numberWrong}</h6>
+
+          <h4>{guessedWord}</h4>
         </section>
       </div>
     );
