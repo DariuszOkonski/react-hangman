15e223caf2ee388cd5511994c21e458930b1ff5a
Dariusz Okonski <darek200180@gmail.com>
2020-01-10 13:19:31 +0100

    change stateless component into statefull component and write logic for only one click possibility

--- src/components/alphabet-buttons/single-button/SingleButton.component.jsx
+++ src/components/alphabet-buttons/single-button/SingleButton.component.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SingleButton.styles.scss';
 
-const SingleButton = (props) => {
-  return (
-    <div className="single-button">
-      {props.letter}
-    </div>
-  );
+class SingleButton extends Component {
+  state = {
+    clicked: false
+  }
+
+  handleClick = () => {
+    this.props.handleAlphabetButton(this.props.letter)
+
+    this.setState({ clicked: true });
+  }
+
+  render() {
+    return (
+      <div
+        className={`single-button ${this.state.clicked && "marked"}`}
+        onClick={!this.state.clicked ? this.handleClick : null}
+      >
+        {this.props.letter}
+      </div>
+    );
+  }
 }
 
 export default SingleButton;
