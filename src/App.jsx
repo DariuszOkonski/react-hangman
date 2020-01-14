1723a1f9596d06d8796d896d9c8874137fcda72a
Dariusz Okonski <darek200180@gmail.com>
2020-01-14 22:36:55 +0100

    add: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navigation from './components/navigation/Navigation.component';
 import Footer from './components/footer/Footer.component';
 import Game from './pages/game/Game.page';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Error from './pages/error/Error.page';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="App container">
         <Navigation />
 
-        <Game />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/game">
+            <Game />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route>
+            <Error />
+          </Route>
+        </Switch>
 
         <Footer />
       </div>
