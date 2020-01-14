52b702e95225c5297b1d31a6e20c6edc6ee91d50
Dariusz Okonski <darek200180@gmail.com>
2020-01-14 20:01:53 +0100

    add: write logic for end game

--- src/pages/game/Game.page.jsx
+++ src/pages/game/Game.page.jsx
@@ -1,33 +1,57 @@
 import React, { Component } from 'react';
 import './Game.styles.scss';
 import imagesArr from '../../utillities/images';
-// import { randomWord } from '../../utillities/words';
 import LeftSection from '../../components/left-section/LeftSection.component';
 import AlphabetButtons from '../../components/alphabet-buttons/AlphabetButtons.component';
 import RestartButton from '../../components/restart-button/RestartButton.component';
 import NumberWrong from '../../components/number-wrong/NumberWrong.component';
 import GuesseWord from '../../components/guesse-world/GuesseWord.component';
 import alphabet from '../../utillities/alphabet.js';
+import Result from '../../components/result/Result';
 
 class Game extends Component {
   state = {
     numberWrong: 0,
     currentWord: 'plently',
     guesseWord: '',
-    alphabet: []
+    alphabet: alphabet,
+    endGame: false,
   }
 
-  handleAlphabetButton = (letter) => {
-    console.log(letter)
+  handleAlphabetButton = (letter, id) => {
 
     console.log(this.state.currentWord.indexOf(letter));
 
     if (this.state.currentWord.indexOf(letter) !== -1) {
       console.log('znalazl')
     } else {
-      console.log('nie znalazł');
+      this.endGame();
+
       this.setState({ numberWrong: this.state.numberWrong + 1 });
     }
+
+    this.blockUsedButton(id);
+  }
+
+  endGame() {
+    if (this.state.numberWrong === imagesArr.length - 2) {
+      this.setState({
+        endGame: true,
+      })
+    }
+  }
+
+  blockUsedButton(id) {
+    let alphabet = [...this.state.alphabet];
+
+    alphabet.forEach(el => {
+      if (el.id === id)
+        el.clicked = true;
+    })
+
+    this.setState({
+      alphabet
+    })
   }
 
   handleResetGuesseWord() {
@@ -57,7 +81,13 @@ class Game extends Component {
 
           <GuesseWord guesseWord={guesseWord} />
 
-          {/* <AlphabetButtons handleAlphabetButton={this.handleAlphabetButton} /> */}
+          {!this.state.endGame ?
+            <AlphabetButtons
+              handleAlphabetButton={this.handleAlphabetButton}
+              alphabet={this.state.alphabet}
+            /> :
+            <Result>You Lose!!!</Result>
+          }
 
           <RestartButton />
         </section>
