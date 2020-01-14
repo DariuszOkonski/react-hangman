bc6d7a2cce330da4f3f87797aad9c1265fd7fd88
Dariusz Okonski <darek200180@gmail.com>
2020-01-14 21:49:33 +0100

    change: remove import uuid

--- src/components/alphabet-buttons/AlphabetButtons.component.jsx
+++ src/components/alphabet-buttons/AlphabetButtons.component.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './AlphabetButtons.styles.scss';
-import uuid from 'uuid/v1';
 import SingleButton from './single-button/SingleButton.component';
 
 const AlphabetButtons = (props) => {
